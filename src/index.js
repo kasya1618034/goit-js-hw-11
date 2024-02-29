c46629d13a60ac277b39bed434dd13e901dc8f27
Katarzyna Filas <katarzyna.filas@gmail.com>
2024-02-29 20:40:12 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
 
-const searchForm = document.querySelector('.search-form');
+const searchQuery = document.querySelector('.search-form');
 
 let page = 1;
 
@@ -14,4 +14,67 @@ const searchParams = new URLSearchParams({
   page: page,
   per_page: 40,
 });
+
+const fetchPhotos = async () => {
+  searchParams.set('q', searchQuery.elements[0].value.split(' ').join('+'));
+  searchParams.set('page', page);
+  const searchResults = await axios.get(
+    `https://pixabay.com/api/?${searchParams}`
+  );
+  return searchResults.data;
+};
+
+fetchPhotos()
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+function renderPhotos(data, addToGallery = false) {
+  if (data.hits.length === 0) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  } else {
+    const markup = data.hits.map(({
+          webformatURL,
+          largeImageURL,
+          tags,
+          likes,
+          views,
+          comments,
+          downloads,
+    }) =>
+      `<div class="photo-card">
+         <img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy"/>
+        <div class="info">
+          <p class="info-item">
+            <b>Likes: ${likes}</b>
+          </p>
+          <p class="info-item">
+            <b>Views: ${views}</b>
+          </p>
+          <p class="info-item">
+            <b>Comments: ${comments}</b>
+          </p>
+          <p class="info-item">
+            <b>Downloads: ${downloads}</b>
+          </p>
+        </div>
+      </div>`
+      )
+      .join('');
+
+    if (addToGallery) {
+      gallery.insertAdjacentHTML('beforeend', markup);
+    } else {
+      gallery.innerHTML = markup;
+    }
+  }
+}
+
+
+
 
