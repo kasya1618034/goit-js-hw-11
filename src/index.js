76975f681ec22986f2bdfad666bfb0a403d34fa1
Katarzyna Filas <katarzyna.filas@gmail.com>
2024-02-29 04:05:36 +0100

    Update structure

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
+
+const searchForm = document.querySelector('.search-form');
+
+let page = 1;
+
+const searchParams = new URLSearchParams({
+  key: '21423072-0f941905d82a42377d360632a',
+  q: '',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: 'true',
+  page: page,
+  per_page: 40,
+});
+
